04f9ba3a5c0ceeae30c613fd7951f117df59efb3
hibamahboob587 <hibamahboob11@gmail.com>
2025-10-25 09:48:07 +0500

    fix

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-
+import tmcLogo from "../assets/TMC.png";
 const Loader = () => {
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-gradient-to-br from-green-500/70 to-white z-50">
       <img
-        src="src\assets\TMC.png"  // ← update this path
+        src={tmcLogo}  // ← update this path
         alt="Loading..."
         className="w-40 h-40 animate-pulse opacity-90"
       />
